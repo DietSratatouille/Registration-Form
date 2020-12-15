d653d2cfc736611954ab9dff22318ec7d65d7d46
Sterling <sterlingtyson97@gmail.com>
2020-12-15 15:39:05 -0500

    Auto stash before merge of "feature/srat" and "origin/feature/edward"

--- Backend/src/main/java/com/example/demo/Repository/UserRepository.java
+++ Backend/src/main/java/com/example/demo/Repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.demo.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+//public interface UserRepository extends JpaRepository<User,Long> {
+//}
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
--- Frontend/reg-form/src/services-HTTP/UserDataServices.js
+++ Frontend/reg-form/src/services-HTTP/UserDataServices.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
 class UserDataServices {
+    //GET Request(s)
+    listUsers(){
+        return axios.get(`http://localhost:8080/listUsers`)
+    }
+
     //POST Request(s)
     createUser(user){
         return axios.post(`http://localhost:8080/create`, user)
@@ -8,3 +13,4 @@ class UserDataServices {
 }
 
 export default new UserDataServices()
+
